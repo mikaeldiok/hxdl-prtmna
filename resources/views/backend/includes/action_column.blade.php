adf9bef5a3877b7dab3cb9feb9c81aa61113b2d3
mikaeldiok <mikaeldiok@gmail.com>
2022-11-21 18:28:33 +0700

    alter superadmin view for datatable

--- resources/views/backend/includes/action_column.blade.php
+++ resources/views/backend/includes/action_column.blade.php
@@ -1,8 +1,10 @@
 <div class="text-right">
     @if(Auth::user()->hasRole("pengawas"))
         <x-buttons.show class="m-1"  route='{!!route("backend.$module_name.show", $data)!!}' title="{{__('Show')}} {{ ucwords(Str::singular($module_name)) }}" small="true" />
-    @elseif(Auth::user()->hasRole("hsse"))
+    @elseif(Auth::user()->hasRole("hsse"))        
         <x-buttons.show class="m-1"  route='{!!route("backend.$module_name.show-hsse", $data)!!}' title="{{__('Show')}} {{ ucwords(Str::singular($module_name)) }}" small="true" />
-    @else
+    @elseif(Auth::user()->hasRole("super admin"))
+        <x-buttons.show class="m-1"  route='{!!route("backend.$module_name.show", $data)!!}' title="{{__('Show')}} {{ ucwords(Str::singular($module_name)) }}" small="true" />
+        <x-buttons.edit route='{!!route("backend.$module_name.edit", $data)!!}' title="{{__('Edit')}} {{ ucwords(Str::singular($module_name)) }}" small="true" />
     @endif
 </div>
